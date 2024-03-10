30b616aed16bab28f863693462cc02d1cc15999e
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-03-10 18:28:44 -0300

    Refatorando testes unitários

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,20 +1,24 @@
-
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
-import { AnswersRepository } from "../repositories/answers-repository"
-import { Answer } from "../../enterprise/entities/answer"
-
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
   questionId: string
   content: string
 }
 
-export class AnswerQuestionUseCase {
-  constructor(
-    private answersRepository: AnswersRepository,
-  ) {}
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer
+}
 
-  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityID(instructorId),
@@ -23,6 +27,8 @@ export class AnswerQuestionUseCase {
 
     await this.answersRepository.create(answer)
 
-    return answer
+    return {
+      answer,
+    }
   }
 }
